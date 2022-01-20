4e1e92f09cf2814f48afbde12fa2e08fa5409e9f
Annamari Aalto <annamari.aalto@hotmail.com>
2022-01-20 18:48:34 +0200

    Checking the player's games. Create project structure.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,17 +3,25 @@ package org.example;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.sun.security.jgss.GSSUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
+@SpringBootApplication
 public class App {
 
     private static final String POST_API_URL = "https://bad-api-assignment.reaktor.com/rps/history";
 
     public static void main( String[] args ) throws IOException, InterruptedException {
+        //SpringApplication.run(App.class, args);
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
@@ -24,17 +32,18 @@ public class App {
 
         //parse JSON into objects
         ObjectMapper objectMapper = new ObjectMapper();
-        //ObjectNode objectNode = objectMapper.createObjectNode();
-
         JsonNode jsonNode = objectMapper.readTree(response.body());
 
+        //Handle the cursor
         JsonNode cursor = jsonNode.get("cursor");
-        System.out.println("Firstly: " + cursor);
+        String cursorText = cursor.asText();
+        System.out.println("Here is the cursor: " + cursorText);
 
+        //Handle the data
         JsonNode data = jsonNode.get("data");
-        //System.out.println("Secondly: " + data);
 
-        traverse(data);
+        //traverse(data);
+        personalResults(data);
 
     }
     //Traverse through array
@@ -49,4 +58,47 @@ public class App {
             System.out.println("Invalid element ID");
         }
     }
+
+    //To look individual's results
+    public static void personalResults (JsonNode root) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the player's full name: ");
+        String merkki = scanner.nextLine();
+
+        if (root.isArray()) {
+            int games = 0;
+            ArrayNode arrayNode = (ArrayNode) root;
+            for (int i = 0 ; i < arrayNode.size(); i++) {
+                JsonNode arrayElement = arrayNode.get(i);
+
+                if (arrayElement.has("type")) {
+                    JsonNode player1 = arrayElement.get("playerA");
+                    JsonNode player2 = arrayElement.get("playerB");
+
+                    JsonNode name1 = player1.get("name");
+                    String nameText1 = name1.asText();
+
+                    JsonNode name2 = player2.get("name");
+                    String nameText2 = name2.asText();
+
+                    //System.out.println(nameText1);
+                    //System.out.println(nameText2);
+
+                    if (nameText1.equals(merkki) || nameText2.equals(merkki)){
+                        System.out.println();
+                        //System.out.println("***** Player A: ***** " + nameText1);
+                        //System.out.println("***** Player B: ***** " + nameText2);
+                        games++;
+                    }
+
+                } else {
+                    System.out.println("No results.");
+                }
+            }
+            System.out.println("The player " + merkki + " played " + games + " games.");
+        }
+
+    }
+
+
 }
--- src/main/java/org/example/HistoryResult.java
+++ src/main/java/org/example/HistoryResult.java
@@ -1,7 +1,5 @@
 package org.example;
 
-//import java.lang.reflect.Array;
-
 public class HistoryResult {
     private String cursor;
     JSONObject data = new JSONObject();
